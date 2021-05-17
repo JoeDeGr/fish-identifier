5b4c5f988f7ac58e922b12c454cd64e161648909
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-17 14:18:41 -0400

    creating user-ligin functionality.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import NewUserInput from './components/NewUserInput'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <NewUserInput/>
       </header>
     </div>
   );
--- src/components/NewUserInput.js
+++ src/components/NewUserInput.js
@@ -14,6 +14,7 @@ class NewUserInput extends Component {
     }
 
     handleOnChange = event => {
+        console.log(event.target.name)
         this.setState({
             [event.target.name]: event.target.value
         })
@@ -21,39 +22,50 @@ class NewUserInput extends Component {
 
     handleOnSubmit = event => {
         event.preventDefault()
-        if (!this.state.username || !this.state.email) return
-        if (!(this.state.password === this.state.passwordConfirmation) || (this.state.password == '')) return
+        console.log(this.state)
+        // if (!this.state.username || !this.state.email) return
+        // if (!(this.state.password === this.state.passwordConfirmation) || (this.state.password === '')) return
 
-        this.props.handleLogin(this.state)
+        // this.props.handleLogin(this.state)
     }
 
     render() {
         return(
             <div>
                 <form onSubmit = {this.handleOnSubmit}>
+                    <label>Username</label>
                     <input
+                        name = "username"
                         type = "text"
-                        onChange = { this.handleOnChange}
-                        value = { this.state.username}
+                        onChange = {(e) => this.handleOnChange(e) }
+                        value = { this.state.username }
                     />
+                    <label>Name</label>
                     <input
+                        name = "name"
                         type = "text"
-                        onChange = { this.handleOnChange}
-                        value = { this.state.name}
+                        onChange = {(e) => this.handleOnChange(e) }
+                        value = { this.state.name }
                     />
+                    <label>Email</label>
                     <input
+                        name = "email"
                         type = "text"
-                        onChange = { this.handleOnChange}
+                        onChange = {(e) => this.handleOnChange(e) }
                         value = { this.state.email}
                     />
+                    <label>Password</label>
                     <input
+                        name = "password"
                         type = "text"
-                        onChange = { this.handleOnChange}
+                        onChange = {(e) => this.handleOnChange(e) }
                         value = { this.state.password}
                     />
+                    <label>Confirm Password Please</label>
                     <input
+                        name = "passwordConfirmation"
                         type = "text"
-                        onChange = { this.handleOnChange}
+                        onChange = {(e) => this.handleOnChange(e) }
                         value = { this.state.passwordConfirmation}
                     />
                     <input type="submit"/>
@@ -65,4 +77,4 @@ class NewUserInput extends Component {
 
 }
 
-export default connect(null, mapDispatchToProps)(NewUserInput)
+export default connect()(NewUserInput)
