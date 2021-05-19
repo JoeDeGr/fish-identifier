6964ada3332daf595ce86aa696b99142d99ed94b
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-19 19:18:45 -0400

    adding react router to switch login flow.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './containers/Login'
 import UsersContainer from './containers/UsersContainer'
+// import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+      {/* <Router>
+        <main>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/Genus">Genus</a></li>
+              <li><a href="/species">Species</a></li>
+              <li><a href="/user/following">MyFish</a></li>
+            </ul>
+          </nav>
+          <Route path="/" container={UsersContainer}></Route>
+        </main>
+      </Router> */}
+      
       <Login />
       <UsersContainer/>
       </header>
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import NewUserInput from '../components/NewUserInput';
 import UserLogin from '../components/UserLogin';
 import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class Login extends Component {
     render() {
         return(
             <div>
-                <NewUserInput/>
-                <UserLogin/>
+                <Router>
+                    <div class="login">
+                        <nav>
+                            <h1> Welcome! </h1>
+                            <ul>
+                                <Link to="/newuser">Create A New Profile</Link><br></br>
+                                <Link to="/login">Login</Link>
+                            </ul>
+                        </nav>
+                    </div>
+                    <Switch>
+                        <Route path='/newuser'>
+                            <NewUserInput/>
+                        </Route>
+                        <Route path="/login">
+                            <UserLogin/>
+                        </Route>
+                    </Switch>
+                </Router> 
             </div>
         );
     }
