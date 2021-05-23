3844850de003c7876d450102eef98d6f63269696
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-23 15:42:10 -0400

    added reworked Private Route Component.

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route, Redirect, useLocation } from 'react-router-dom';
+//create a private route to wrap components we want hidden.
+export const PrivateRoute = ({ component: Component, ...rest }){
+
+    <Route {...rest} render={props => (
+        localStorage.getItem('user')
+        ? <Component {...props} />
+        : <redirect to={{pathname: '/login', state: {from: location}}}/>
+        )}/>
+  }
