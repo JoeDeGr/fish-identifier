4ee3b824ee8d7d14910abc446e781f79ee98c135
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-05-23 15:46:52 -0400

    working on errors in private route.

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Route, Redirect, useLocation } from 'react-router-dom';
 //create a private route to wrap components we want hidden.
-export const PrivateRoute = ({ component: Component, ...rest }){
+export const PrivateRoute = ({ component: Component, ...rest }) => (
 
     <Route {...rest} render={props => (
         localStorage.getItem('user')
         ? <Component {...props} />
-        : <redirect to={{pathname: '/login', state: {from: location}}}/>
+        : <Redirect to={{pathname: '/login', state: {from: props.location}}}/>
         )}/>
-  }
+)
