1c85e0528adf28773ddf17afac91b6679a5713e2
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-06-03 10:25:21 -0400

    removing debuggers and drying up code in login flow.

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -2,29 +2,13 @@ import React, { Component } from 'react';
 import LoginNav from '../nav/LoginNav'
 import NewUserInput from '../components/User/NewUserInput';
 import UserLogin from '../components/User/UserLogin';
-import {userActions} from '../actions/userActions';
+import { userActions } from '../actions/userActions';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 class LoginContainer extends Component {
 
     render() {
-        // const createUser = (data) => { 
-        //     localStorage.setItem('user', JSON.stringify(data.user))
-        // };
-        // const setToken = (data) => {localStorage.setItem('token', JSON.stringify(data.user.jwt))};
-
-        // const get = (options, location) => {
-        //     fetch('http://localhost:3000' + location, options)
-        //     .then(resp => resp.json())
-        //     .then(data => {
-        //         console.log(data);
-        //         createUser(data);
-        //         setToken(data);
-        //         // <Redirect to={{pathname: '/user' }}/>
-                
-        //     })
-        // }
         return(
             <div>
                 <Router>
