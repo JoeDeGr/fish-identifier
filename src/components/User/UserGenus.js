a8128e7dfb0b177379aaf3c49223710304f86957
Joseph DeGregorio <DeGregorio.Joseph@Gmail.com>
2021-06-16 10:55:13 -0400

    converted user/genus and user/species to functional components.

--- src/components/User/UserGenus.js
+++ src/components/User/UserGenus.js
@@ -2,31 +2,27 @@ import React from 'react';
 import Genus from '../Genus/Genus'
 import { connect } from 'react-redux'
 
-class UserGenus extends React.Component {
+function UserGenus ({userGenus}){
 
-    render() {
- 
-        const { userGenus } = this.props;
-        const genusList =  ((!!userGenus) ? userGenus.map( genus => {
-            return( 
-            <li id={genus.id}>
-                <Genus
-                    key = {genus.id}
-                    genus = {genus}
-                    // addGenusToUser = {addGenusToUser}
-                    // removeGenus = {removeGenus}
-                />
-            </li>
-            )
-        }) : (<p>Add Some <strong><a href="/genus">Genera</a></strong> to your life! </p>));
-        return(
-            <div> 
-                <ul>
-                    {genusList}
-                </ul>
-            </div>
+    const genusList =  ((!!userGenus) ? userGenus.map( genus => {
+        return( 
+        <li id={genus.id}>
+            <Genus
+                key = {genus.id}
+                genus = {genus}
+                // addGenusToUser = {addGenusToUser}
+                // removeGenus = {removeGenus}
+            />
+        </li>
         )
-    }
+    }) : (<p>Add Some <strong><a href="/genus">Genera</a></strong> to your life! </p>));
+    return(
+        <div> 
+            <ul>
+                {genusList}
+            </ul>
+        </div>
+    )
 }
 
 export default  connect ()(UserGenus)
--- src/components/User/UserSpecies.js
+++ src/components/User/UserSpecies.js
@@ -2,32 +2,28 @@ import React from 'react';
 import Species from '../Species/Species'
 import { connect } from 'react-redux'
 
-class UserSpecies extends React.Component {
+function UserSpecies ({userSpecies}) {
 
-    render() {
- 
-        const { userSpecies } = this.props;
-        const speciesList = ( (!!userSpecies) ? userSpecies.map( species => {
-            return(
-            <li id={species.id}>
-                <Species
-                    key = {species.id}
-                    species = {species}
-                    // addSpeciesToUser = {addSpeciesToUser}
-                    // removeSpecies = {removeSpecies}
-                />
-            </li>
-            )
-        }) : (<p>Add Some <a href="/species">Species!</a></p>));
+    const speciesList = ( (!!userSpecies) ? userSpecies.map( species => {
         return(
-            <div> 
-                <ul>
-                    {speciesList} 
-                </ul>
-            </div>
-            
+        <li id={species.id}>
+            <Species
+                key = {species.id}
+                species = {species}
+                // addSpeciesToUser = {addSpeciesToUser}
+                // removeSpecies = {removeSpecies}
+            />
+        </li>
         )
-    }
+    }) : (<p>Add Some <a href="/species">Species!</a></p>));
+    return(
+        <div> 
+            <ul>
+                {speciesList} 
+            </ul>
+        </div>
+        
+    )
 }
 
 export default  connect ()(UserSpecies)
